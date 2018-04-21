769feddb59276d79b521d14c1f21fc81a7d23293
Owais <owaisiqbal91@yahoo.com>
2018-04-20 20:45:46 -0400

    Adding max kernel, fixing some bugs

--- quadpic/kernel.cu
+++ quadpic/kernel.cu
@@ -4,13 +4,16 @@
 
 #include <stdio.h>
 
+#define noOfIterations 2500
+#define noOfQuads noOfIterations*3
+
 #define rows 8//image rows
 #define columns 8//image columns
-#define noOfThreadsInBlock 4
-#define noOfBlocks 4*4
+#define noOfThreadsInBlock 16
+//#define noOfBlocks 4
 #define noOfThreadInBlockBy2 noOfThreadsInBlock/2
-#define gridDim3 dim3(noOfBlocks/2, noOfBlocks/2)
-#define blockDim3 dim3(noOfThreadInBlockBy2, noOfThreadInBlockBy2)
+//#define gridDim3 dim3(noOfBlocks/2, noOfBlocks/2)
+//#define blockDim3 dim3(noOfThreadInBlockBy2, noOfThreadInBlockBy2)
 
 cudaError_t generateOutputWithCuda(int *c, const int *a, unsigned int size);
 
@@ -29,7 +32,7 @@ __global__ void getAverageKernel(const int *a, int *mutex, float* average, int q
 	int threadAbsX = (threadIdx.x + blockDim.x*blockIdx.x) + quadStartX;
 	int threadAbsY = (threadIdx.y + blockDim.y*blockIdx.y) + quadStartY;
 
-	if (threadAbsX <= quadEndX && threadAbsY <= quadEndY) {
+	if (threadAbsX <= quadEndX && threadAbsY <= quadEndY /*&& blockIdx.x == 0 && blockIdx.y == 0*/) {
 		__shared__ int sum[noOfThreadInBlockBy2];
 		int index = getAbsoluteIndex(threadIdx.x, threadIdx.y, quadStartX, quadStartY);
 		int threadIndex = threadIdx.x + threadIdx.y*blockDim.x;//local 1d index into the block
@@ -40,14 +43,16 @@ __global__ void getAverageKernel(const int *a, int *mutex, float* average, int q
 				if (i == localKernelNoOfThreadsHalf) {//first iteration
 
 					sum[threadIndex] = a[index];
-					int x2 = (threadIdx.x + i) % blockDim.x;
-					int y2 = (threadIdx.y + i) / blockDim.x;
+					//printf("\nSum[%d] = %d", threadIndex, index);
+					int x2 = threadIdx.x;// + i) % blockDim.x;
+					int y2 = threadIdx.y + (i / blockDim.x);
 					int threadAbsX2 = (x2 + blockDim.x*blockIdx.x) + quadStartX;
 					int threadAbsY2 = (y2 + blockDim.y*blockIdx.y) + quadStartY;
 					if (threadAbsX2 <= quadEndX && threadAbsY2 <= quadEndY) {
 						int index2 = getAbsoluteIndex(x2, y2, quadStartX, quadStartY);
 
 						sum[threadIndex] += a[index2];
+						//printf("\nSum[%d] 2nd = %d, threadIdy: %d, i: %d, blockDimy: %d, calc: %d, y2: %d", threadIndex, index2, threadIdx.y, i, blockDim.x, (i+threadIdx.y)/blockDim.x, y2);
 					}
 				}
 				else {
@@ -63,13 +68,14 @@ __global__ void getAverageKernel(const int *a, int *mutex, float* average, int q
 		if (threadIndex == 0) {
 			//int blockIndex = blockIdx.x + blockIdx.y*blockDim.x;
 			while (atomicCAS(mutex + quadNo, 0, 1) != 0);  //lock
+			printf("\nBlock sum is %d", sum[0]);
 			average[quadNo] += (float)sum[0] / ((quadEndX - quadStartX + 1)*(quadEndY - quadStartY + 1));
 			atomicExch(mutex + quadNo, 0);  //unlock
 		}
 	}
 }
 
-__global__ void getScoreKernel(const int *a, int *mutex, float* average, float* score, int quadNo, int quadStartX, int quadStartY, int quadEndX, int quadEndY) {
+__global__ void getScoreAndPaintKernel(const int *a, int *mutex, float* average, float* score, int quadNo, int quadStartX, int quadStartY, int quadEndX, int quadEndY) {
 
 	int threadAbsX = (threadIdx.x + blockDim.x*blockIdx.x) + quadStartX;
 	int threadAbsY = (threadIdx.y + blockDim.y*blockIdx.y) + quadStartY;
@@ -87,8 +93,8 @@ __global__ void getScoreKernel(const int *a, int *mutex, float* average, float* 
 
 					error[threadIndex] = pow(a[index] - avg, 2);
 
-					int x2 = (threadIdx.x + i) % blockDim.x;
-					int y2 = (threadIdx.y + i) / blockDim.x;
+					int x2 = threadIdx.x;// + i) % blockDim.x;
+					int y2 = threadIdx.y + (i / blockDim.x);
 					int threadAbsX2 = (x2 + blockDim.x*blockIdx.x) + quadStartX;
 					int threadAbsY2 = (y2 + blockDim.y*blockIdx.y) + quadStartY;
 					if (threadAbsX2 <= quadEndX && threadAbsY2 <= quadEndY) {
@@ -114,24 +120,92 @@ __global__ void getScoreKernel(const int *a, int *mutex, float* average, float* 
 	}
 }
 
-__global__ void kernelToRuleThemAll(int *c, const int *a, int *mutex, float* average, float *score) {
-	getAverageKernel <<<dim3(4,4), blockDim3>>>(a, mutex, average, 1, 0, 0, 6, 6);
-	cudaDeviceSynchronize();
-	getScoreKernel << <dim3(4, 4), blockDim3 >> >(a, mutex, average, score, 1, 0, 0, 6, 6);
+__global__ void getMaxScoreKernel(float* globalScores, int* maxScoreIndex, int currentTotalQuads) {
+	__shared__ float maxValues[512];
+	__shared__ int maxIndex[512];
+
+	int localKernelNoOfThreadsHalf = blockDim.x / 2;
+	unsigned int i = localKernelNoOfThreadsHalf;
+
+	int threadGlobalIndex = blockIdx.x*blockDim.x + threadIdx.x;
+
+	if (threadGlobalIndex < currentTotalQuads) {
+		while (i != 0) {
+			if (threadIdx.x < i) {
+				if (i == localKernelNoOfThreadsHalf) {//first iteration
+
+					maxValues[threadIdx.x] = globalScores[threadGlobalIndex];
+					maxIndex[threadIdx.x] = threadGlobalIndex;
+
+					int threadGlobalIndex2 = threadGlobalIndex + i;
+					
+					if (threadGlobalIndex2 < currentTotalQuads) {
+						if (globalScores[threadGlobalIndex2] > maxValues[threadIdx.x]) {
+							maxValues[threadIdx.x] = globalScores[threadGlobalIndex2];
+							maxIndex[threadIdx.x] = threadGlobalIndex2;
+						}
+					}
+				}
+				else {
+					if (maxValues[threadIdx.x + i] > maxValues[threadIdx.x]) {
+						maxValues[threadIdx.x] = maxValues[threadIdx.x + i];
+						maxIndex[threadIdx.x] = maxIndex[threadIdx.x + i];
+					}
+				}
+			}
+
+			__syncthreads();
+			i /= 2;
+		}
+
+		if (threadIdx.x == 0) {
+			printf("\nMax value is %f, %d", maxValues[0], maxIndex[0]);
+		}
+	}
+}
+
+__device__ struct quad {
+	short int startX;
+	short int startY;
+	short int endX;
+	short int endY;
+};
+
+
+__global__ void kernelToRuleThemAll(int *c, const int *a, int *mutex, float* average, float *score, float* globalScores, int* maxScoreIndex) {
+
+	quad quads[noOfQuads];
+
+	cudaStream_t s1;
+	cudaStreamCreateWithFlags(&s1, cudaStreamNonBlocking);
+
+	//TODO shared memory size
+	//getAverageKernel <<<dim3(4,4), dim3(4,4), 0, s1>>>(a, mutex, average, 1, 0, 0, 7, 7);
+	//getScoreAndPaintKernel << <dim3(4, 4), dim3(4,4), 0, s1>> >(a, mutex, average, score, 1, 0, 0, 7, 7);
 	//TODO remember to square root error
 
-	cudaDeviceSynchronize();
-	printf("Average is %f", average[1]);
-	printf("\nScore is %f", score[1]);
+	//find max
+	globalScores[0] = 1;
+	globalScores[1] = 2;
+	globalScores[2] = 3;
+	globalScores[3] = 4;
+	globalScores[4] = 5;
+	globalScores[5] = 6;
+	getMaxScoreKernel<<<1, 8>>>(globalScores, maxScoreIndex, 6);
 
-	//have to parallelize in streams the different kernel launches
-	//one stream will calc average then calc deviation
-	//have to use cudaDeviceSynchronize() after deviation calculations to get minimum from all
-	//paint after all iterations are done
+	/*cudaDeviceSynchronize();
+	printf("\nAverage is %f", average[1]);
+	printf("\nScore is %f", score[1]);*/
 }
 
 int main()
 {
+	/*size_t free, total;
+	cudaMemGetInfo(&free,
+		&total
+	);
+	printf("\nCUDA FREE: %zu, TOTAL %zu", free, total);*/
+
     const int arraySize = rows*columns;
     /*const int imageData[arraySize] = { 0, 1,	4, 5,
 									   2, 3,	6, 7,	
@@ -145,12 +219,12 @@ int main()
 	for (id = 0; id < 64; id++) {
 		imageData[id] = id;
 	}
-    int c[noOfBlocks] = { 0 };
+    int c[rows*columns] = { 0 };
 
     // Add vectors in parallel.
     cudaError_t cudaStatus = generateOutputWithCuda(c, imageData, arraySize);
     if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addWithCuda failed!");
+        fprintf(stderr, "generateOutputWithCuda failed!");
         return 1;
     }
 
@@ -173,6 +247,8 @@ cudaError_t generateOutputWithCuda(int *c, const int *a, unsigned int size)
 	int *dev_mutex = 0;
 	float *dev_average = 0;
 	float *dev_score = 0;
+	float *dev_globalScores = 0;
+	int *dev_maxScoreIndex = 0;
     cudaError_t cudaStatus;
 
     // Choose which GPU to run on, change this on a multi-GPU system.
@@ -213,6 +289,18 @@ cudaError_t generateOutputWithCuda(int *c, const int *a, unsigned int size)
 		goto Error;
 	}
 
+	cudaStatus = cudaMalloc((void**)&dev_globalScores, noOfQuads * sizeof(float));
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc failed!");
+		goto Error;
+	}
+
+	cudaStatus = cudaMalloc((void**)&dev_maxScoreIndex, sizeof(int));
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc failed!");
+		goto Error;
+	}
+
     // Copy input vectors from host memory to GPU buffers.
     cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
     if (cudaStatus != cudaSuccess) {
@@ -221,13 +309,13 @@ cudaError_t generateOutputWithCuda(int *c, const int *a, unsigned int size)
     }
 
     // Launch a kernel on the GPU.
-	kernelToRuleThemAll << <1, 1 >> >(dev_c, dev_a, dev_mutex, dev_average, dev_score);
+	kernelToRuleThemAll << <1, 1 >> >(dev_c, dev_a, dev_mutex, dev_average, dev_score, dev_globalScores, dev_maxScoreIndex);
     //getAverageKernel<<<gridDim3, blockDim3>>>(dev_c, dev_a, dev_mutex, dev_average, 0);
 
     // Check for any errors launching the kernel
     cudaStatus = cudaGetLastError();
     if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+        fprintf(stderr, "Main kernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
         goto Error;
     }
     
@@ -235,12 +323,12 @@ cudaError_t generateOutputWithCuda(int *c, const int *a, unsigned int size)
     // any errors encountered during the launch.
     cudaStatus = cudaDeviceSynchronize();
     if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching kernelToRuleThemAll!\n", cudaStatus);
         goto Error;
     }
 
     // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(c, dev_c, noOfBlocks * sizeof(int), cudaMemcpyDeviceToHost);
+    cudaStatus = cudaMemcpy(c, dev_c, rows*columns * sizeof(int), cudaMemcpyDeviceToHost);
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMemcpy failed!");
         goto Error;
@@ -267,3 +355,4 @@ Error:
 }
 
 //one kernel to rule them all, app store near you
+// reduction, dynamic parallelism, streams
